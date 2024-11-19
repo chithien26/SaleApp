b88af9ecff1bd76b499758246372c925cd978dc3
Your Name <lechithien26@gmail.com>
2024-11-20 01:51:03 +0700

    Start project

---
+++ SaleApp/src/main/java/com/lct/SaleApp/SaleAppApplication.java
@@ -0,0 +1,13 @@
+package com.lct.SaleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleAppApplication.class, args);
+	}
+
+}
