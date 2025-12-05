d3129338e00f268a3defdce173c3121291811a8b
chithien26 <lechithien26@gmail.com>
2025-12-05 15:02:47 +0700

    modify: project structure to modules

---
+++ SaleApp/src/main/java/com/lct/SaleApp/order/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.lct.SaleApp.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+}
